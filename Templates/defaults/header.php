3f99b15703c01e90340b5bbb5961830dc1bdcf69
maks <73646735+MaxymilianHJ@users.noreply.github.com>
2021-11-10 14:49:21 +0100

    homepagina

--- Templates/defaults/header.php
+++ Templates/defaults/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="header-row row lead" >
-        <h1 class="display-2">Health<span class="text-danger">One</span></h1>
+        <a href="home.php"  <h1 class="display-2 text-white" >Health<span class="text-danger">One</span></h1></a>
 
     </div>
 </header>
--- Templates/home.php
+++ Templates/home.php
@@ -11,10 +11,11 @@
             include_once ('defaults/pictures.php');
             ?>
 
-            <h4>Sportcenter HealthOne</h4>
-            Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
-            Bewegen hoort hier ook bij. Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.
-            Sporten is goed voor sterkere spieren en voor de conditie. Sporcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
+             <h4> Sportcenter HealthOne</h4>
+
+            Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen.<br> Goede, gezonde voeding is hiervoor de basis.
+            Bewegen hoort hier ook bij.<br> Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.<br>
+            Sporten is goed voor sterkere spieren en voor de conditie.<br> Sporcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
             <hr>
             <?php
             include_once ('defaults/footer.php');
